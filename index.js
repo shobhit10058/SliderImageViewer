caa9b5778bf35d077a752665a883ca1de9e3ffac
Shobhit Gupta <shobhit.gupta@sprinklr.com>
2022-05-20 22:10:28 +0530

    added styling for image preview part

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-
 let imagesData;
 
-const getData = async() =>{
-    await fetch('./data.json').
-    then(response => response.json())
-    .then(data => {imagesData = data})
-    .catch(err => console.log(err));
-}
+const getData = async () => {
+  await fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      imagesData = data;
+    })
+    .catch((err) => console.log(err));
+};
 await getData();
 console.log(imagesData);
 
 const imageFolder = document.querySelector(".image_folder");
-imagesData.forEach(image => {
-    const imageUrl = document.createElement("li");
-    imageUrl.className = "image_url"
-    imageUrl.innerHTML = `
+imagesData.forEach((image, index) => {
+  const imageUrl = document.createElement("li");
+  imageUrl.className = "image_url";
+  imageUrl.id = `image-${index}`;
+  // imageUrl.addEventListener('click', )
+  imageUrl.innerHTML = `
         <img src="${image.previewImage}" class="image_url_preview"/>
         <p class="image_url_title">${image.title}</p>
     `;
-    imageFolder.append(imageUrl);
+  imageFolder.append(imageUrl);
 });
 
-const imageUrlPointer = 0;
+const imageUrlPointer = 1;
 
 const PreviewSelectedImage = () => {
-    const image = imageFolder[imageUrlPointer];
-    document.querySelector(".image_container").innerHTML = `
+  const image = imagesData[imageUrlPointer];
+  document.querySelector(".image_container").innerHTML = `
         <img src="${image.previewImage}"/>
-        <input type="text" value="${image.title}"
-    `
-}
+        <input type="text" value="${image.title}"/>
+    `;
+};
 
-PreviewSelectedImage()
+PreviewSelectedImage();
+
+document.body.addEventListener("keydown", (event) => {
+  const pressedKey = event.key;
+  if (pressedKey === "ArrowDown") {
+    imageUrlPointer += 1;
+    PreviewSelectedImage();
+  }
+  if (pressedKey === "ArrowUp") {
+    imageUrlPointer -= 1;
+    PreviewSelectedImage();
+  }
+});
