0cc7d7f138936ce46e7fb327c51f89eb1d7fbf9b
Shobhit Gupta <shobhit.gupta@sprinklr.com>
2022-05-20 20:59:30 +0530

    added sidebar and its styling

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ await getData();
 console.log(imagesData);
 
 const imageFolder = document.querySelector(".image_folder");
-
 imagesData.forEach(image => {
     const imageUrl = document.createElement("li");
-    imageUrl.innerHTML = image.title;
+    imageUrl.className = "image_url"
+    imageUrl.innerHTML = `
+        <img src="${image.previewImage}" class="image_url_preview"/>
+        <p class="image_url_title">${image.title}</p>
+    `;
     imageFolder.append(imageUrl);
 });
+
+const imageUrlPointer = 0;
+
+const PreviewSelectedImage = () => {
+    const image = imageFolder[imageUrlPointer];
+    document.querySelector(".image_container").innerHTML = `
+        <img src="${image.previewImage}"/>
+        <input type="text" value="${image.title}"
+    `
+}
+
+PreviewSelectedImage()
